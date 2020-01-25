3197bf09d32d9c7922fce43dce6a4543ff78e814
Arttu Janhunen <arttu.janhunen@gmail.com>
2020-01-25 14:21:28 +0200

    Modified signup.js to take url from .env file

--- src/services/signup.js
+++ src/services/signup.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-const baseUrl = '/api/signup'
+const baseUrl = `${process.env.REACT_APP_API_URL}/signup`
 
 const signup = async credentials => {
   const response = await axios.post(baseUrl, credentials)
+  console.log(baseUrl)
   return response.data
 }
 
