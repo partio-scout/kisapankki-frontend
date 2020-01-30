16b0a259722875cbe3b26022b03ce1b2acac8796
anemev <59803897+anemev@users.noreply.github.com>
2020-01-30 20:54:00 +0200

    Show error message if name/username/password is less than 3 characters

--- src/components/SignUp.test.js
+++ src/components/SignUp.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import SignUp from './SignUp'
 
 afterEach(cleanup)
@@ -31,4 +31,62 @@ describe('<SignUp />', () => {
       'Rekisteröidy',
     )
   })
+
+  test('shows error message if name is less than 3 characters', () => {
+    const button = component.container.querySelector('.signup-button')
+    fireEvent.click(button)
+  
+    const div = component.container.querySelector('.signup-form')
+    expect(div).toHaveTextContent('Nimessä pitää olla vähintään 3 kirjainta')
+  })
+
+  test('does not show error message if name is at least 3 characters', () => {
+    const input = component.container.querySelector('.name')
+    fireEvent.change(input, { target: { value: 'nimi' } })
+
+    const button = component.container.querySelector('.signup-button')
+    fireEvent.click(button)
+  
+    const div = component.container.querySelector('.signup-form')
+    expect(div).not.toHaveTextContent('Nimessä pitää olla vähintään 3 kirjainta')
+  })
+
+  test('shows error message if username is less than 3 characters', () => {
+    const button = component.container.querySelector('.signup-button')
+    fireEvent.click(button)
+  
+    const div = component.container.querySelector('.signup-form')
+    expect(div).toHaveTextContent('Käyttäjätunnuksessa pitää olla vähintään 3 kirjainta')
+  })
+
+  test('does not show error message if username is at least 3 characters', () => {
+    const input = component.container.querySelector('.username')
+    fireEvent.change(input, { target: { value: 'käyttäjätunnus' } })
+
+    const button = component.container.querySelector('.signup-button')
+    fireEvent.click(button)
+  
+    const div = component.container.querySelector('.signup-form')
+    expect(div).not.toHaveTextContent('Käyttäjätunnuksessa pitää olla vähintään 3 kirjainta')
+  })
+
+  test('shows error message if password is less than 3 characters', () => {
+    const button = component.container.querySelector('.signup-button')
+    fireEvent.click(button)
+  
+    const div = component.container.querySelector('.signup-form')
+    expect(div).toHaveTextContent('Salasanassa pitää olla vähintään 3 kirjainta')
+  })
+
+  test('does not show error message if password is at least 3 characters', () => {
+    const input = component.container.querySelector('.password')
+    fireEvent.change(input, { target: { value: 'salasana' } })
+
+    const button = component.container.querySelector('.signup-button')
+    fireEvent.click(button)
+  
+    const div = component.container.querySelector('.signup-form')
+    expect(div).not.toHaveTextContent('Salasanassa pitää olla vähintään 3 kirjainta')
+  })
+
 })
