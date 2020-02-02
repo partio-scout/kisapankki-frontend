333483bdd0a27b3a7ca5968ccad3352b9e9d0489
NooraVino <noora.virolainen@yahoo.com>
2020-02-02 21:22:06 +0200

    language-tests

---
+++ src/components/Language.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, cleanup } from '@testing-library/react'
+import Language from './Language'
+
+afterEach(cleanup)
+
+describe('<Language />', () => {
+  let component
+
+  beforeEach(() => {
+    component = render(
+      <Language />,
+    )
+  })
+
+  test('renders heading', () => {
+    expect(component.container).toHaveTextContent(
+      'Lisää kieli'
+    )
+  })
+
+  test('renders input for language', () => {
+    const inputs = component.container.querySelectorAll('input')
+    expect(inputs.length).toBe(1)
+  })
+
+  test('renders submit button', () => {
+    const button = component.container.querySelector('.language-add-button')
+    expect(button).toHaveTextContent(
+      'Lisää kieli',
+    )
+  })
+
+})
