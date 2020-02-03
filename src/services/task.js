ac8e1a28a8b1d3a3343a32359c1f31d88e5d48fd
anemev <59803897+anemev@users.noreply.github.com>
2020-02-03 20:42:14 +0200

    Fix authorization in task service

--- src/services/task.js
+++ src/services/task.js
@@ -4,8 +4,13 @@ import tokenService from './token'
 const baseUrl = `${process.env.REACT_APP_API_URL}/task`
 
 const addtask = async (task) => {
-  const config = {
-    headers: { Authorization: tokenService.getToken() }
+  let config = null
+  let token = tokenService.getToken()
+
+  if (token) {
+    config = {
+      headers: { Authorization: token }
+    }
   }
 
   const response = await axios.post(baseUrl, task, config)
