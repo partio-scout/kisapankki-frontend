94a42c5447918e3948bda572472a267911c46689
NooraVino <noora.virolainen@yahoo.com>
2020-02-21 14:25:26 +0200

    filter

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,18 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import taskService from '../services/task'
+import ageGroupService from '../services/ageGroup'
+import ruleService from '../services/rule'
+import categoryService from '../services/category'
 import Notification from './Notification'
+import Select from 'react-select'
 
 
-const TaskList = ({user}) => {
+
+const TaskList = ({ user }) => {
+  const [allTasks, setAllTasks] = useState([])
   const [tasks, setTasks] = useState([])
   const [errorMessage, setErrorMessage] = useState(null)
+  const [selectedRules, setSelectedRule] = useState('')
+  const [selectedAgeGroups, setSelectedAgeGroup] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [categories, setCategories] = useState([])
+  const [ageGroups, setAgeGroups] = useState([])
+  const [rules, setRules] = useState([])
+  const [isClearable, setIsClearable] = useState(true)
+
 
   useEffect(() => {
     taskService.getTasks().then((response) => {
       setTasks(response)
+      setAllTasks(response)
     })
   }, [])
+
+  useEffect(() => {
+    categoryService.getCategories().then((response) => {
+      setCategories(response)
+    })
+  }, [])
+
+  useEffect(() => {
+    ageGroupService.getAgeGroups().then((response) => {
+      setAgeGroups(response)
+    })
+  }, [])
+
+  useEffect(() => {
+    ruleService.getRules().then((response) => {
+      setRules(response)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (selectedCategory && selectedCategory.id) {
+      const s = allTasks.filter(task => task.category.id === selectedCategory.id)
+      setTasks(s)
+    }
+  }, [selectedCategory])
 
   const handleDelete = async (task) => {
     try {
@@ -26,9 +66,60 @@ const TaskList = ({user}) => {
     }
   }
 
+  const handleCategoryFiltering = (category) => {
+    if (category) {
+    setSelectedCategory(category)
+    } else {
+      setSelectedCategory("")
+      setTasks(allTasks)
+    }
+
+  }
+
+  const handleAgeGroupFiltering = (ageGroup) => {
+    setSelectedAgeGroup(ageGroup)
+
+  }
+
+  const handleRuleFiltering = (rule) => {
+    setSelectedRule(rule)
+  }
+
+  // const toggleClearable = () =>
+  //   setIsClearable(!isClearable)
+
   return (
     <div className="task-list">
       <h1>Kisatehtäväpankki</h1>
+
+      <div className="task-list-select">
+        <Select className="task-list-select"
+          getOptionLabel={option => `${option.category}`}
+          getOptionValue={option => `${option.category}`}
+          onChange={handleCategoryFiltering}
+          options={categories}
+          isClearable={isClearable}
+        />
+
+        <Select className="task-list-select"
+          getOptionLabel={option => `${option.name}`}
+          getOptionValue={option => `${option.name}`}
+          onChange={handleAgeGroupFiltering}
+          options={ageGroups}
+          isClearable={isClearable}
+
+        />
+        <Select className="task-list-select"
+          getOptionLabel={option => `${option.rules}`}
+          getOptionValue={option => `${option.rules}`}
+          onChange={handleRuleFiltering}
+          options={rules}
+          isClearable={isClearable}
+
+        />
+
+      </div>
+
       <Notification message={errorMessage} />
       {tasks.map((task) => (
         <div className="task-list-item" key={task.id}>
@@ -41,7 +132,7 @@ const TaskList = ({user}) => {
           <span>{task.category.category}</span>
 
           {user !== null &&
-              <span><button className="delete-button" onClick={() => handleDelete(task)}>Poista</button></span>
+            <span><button className="delete-button" onClick={() => handleDelete(task)}>Poista</button></span>
           }
         </div>
       ))}
