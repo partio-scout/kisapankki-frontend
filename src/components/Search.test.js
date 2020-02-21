4925942a9876b5f9ddb86ed7d8921613e663e747
Tattimus <samu.tatti@luukku.com>
2020-02-21 13:55:00 +0200

    update tests

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, cleanup } from '@testing-library/react'
-import Search from './SearchTasks'
+import Search from './Search'
 
 afterEach(cleanup)
 
