65f7b5caeeccee06c064c02c68fd3f4c4e89fa9a
anemev <59803897+anemev@users.noreply.github.com>
2020-03-06 14:39:35 +0200

    Import tokenService

--- src/components/TaskListPending.js
+++ src/components/TaskListPending.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import taskService from '../services/task'
 import Notification from './Notification'
+import tokenService from './services/token'
 
 
 const TaskListPending = () => {
