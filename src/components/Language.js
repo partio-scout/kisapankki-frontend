c09b53b17927bef955b11b7f10021e2ae15bea01
NooraVino <noora.virolainen@yahoo.com>
2020-03-11 22:56:53 +0200

    modify and delete

--- src/components/Language.js
+++ src/components/Language.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Notification from './Notification'
 import languageService from '../services/language'
 
 const Language = () => {
-  const [language, setLanguage] = useState('')
+  const [languages, setLanguages] = useState([])
+  const [languageName, setLanguageName] = useState('')
+  const [modifiedLanguageName, setModifiedLanguageName] = useState('')
+  const [modifiedLanguageId, setModifiedLanguageId] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
+  const [modifyVisible, setModifyVisible] = useState(false)
+
+  useEffect(() => {
+    languageService.getLanguages().then((response) => {
+      setLanguages(response)
+    })
+  }, [])
 
   const handleLanguageAdd = async (event) => {
     event.preventDefault()
     try {
-      await languageService.addLanguage({
-        language,
+      const addedLanguage = await languageService.addLanguage({
+        language: languageName,
       })
-      setLanguage('')
+      setLanguageName('')
+      setLanguages(languages.concat(addedLanguage))
     } catch (exception) {
       setErrorMessage('Kielen lisääminen ei onnistunut')
       setTimeout(() => {
@@ -21,23 +32,91 @@ const Language = () => {
     }
   }
 
+  const handleLanguageDelete = async (language) => {
+    try {
+      await languageService.deleteLanguage(language.id)
+      setLanguages(languages.filter(l => l.id !== language.id))
+    } catch (exeption) {
+      setErrorMessage('Jotain meni vikaan')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLanguageModify = async (event) => {
+    event.preventDefault()
+    const modifiedLanguage = { id: modifiedLanguageId, name: modifiedLanguageName }
+    try {
+      await languageService.editLanguage(modifiedLanguage)
+      setModifyVisible(false)
+      setLanguages(languages.map(language => language.id !== modifiedLanguage.id ? language : modifiedLanguage))
+    } catch (exception) {
+      setErrorMessage('Jotain meni vikaan')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+
+
+  const hideWhenVisible = { display: modifyVisible ? 'none' : '' }
+  const showWhenVisible = { display: modifyVisible ? '' : 'none' }
+
+  const handleShowModify = (language) => {
+    setModifyVisible(true)
+    setModifiedLanguageId(language.id)
+    setModifiedLanguageName(language.name)
+  }
+
   return (
     <div className="language-form">
-      <h2>Lisää kieli</h2>
       <Notification message={errorMessage} type="error" />
-      <form onSubmit={handleLanguageAdd}>
+      <form style={hideWhenVisible} onSubmit={handleLanguageAdd}>
         <div>
           <input
             className="language"
             type="text"
-            value={language}
+            value={languageName}
             name="Language"
             placeholder="Kieli"
-            onChange={({ target }) => setLanguage(target.value)}
+            onChange={({ target }) => setLanguageName(target.value)}
           />
+          <button type="submit" className="language-add-button">Lisää</button>
         </div>
-        <button type="submit" className="language-add-button">Lisää</button>
       </form>
+
+      {languages.map((language) => (
+        <div style={hideWhenVisible} className="category-list-item" key={language.id}>{language.name}
+
+          <button style={hideWhenVisible} onClick={() => handleLanguageDelete(language)}>Poista</button>
+          <button style={hideWhenVisible} onClick={() => handleShowModify(language)}>Muokkaa</button>
+
+        </div>))
+      }
+      <div style={showWhenVisible} className="category-form-item">
+        < form onSubmit={handleLanguageModify} >
+          <div>
+            <input
+              className="language"
+              type="text"
+              value={modifiedLanguageName}
+              name="Language"
+              onChange={({ target }) => setModifiedLanguageName(target.value)}
+            />
+            <button type="submit" className="category-save-button">Tallenna</button>
+            <button onClick={() => setModifyVisible(false)}>Peruuta</button>
+          </div>
+
+          <div>
+
+          </div>
+        </form>
+
+      </div>
+
+
     </div>
   )
 }
