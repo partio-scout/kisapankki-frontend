134042108ab59d0559fabd2ccce105b00d9f3e1b
Arttu Janhunen <arttu.janhunen@gmail.com>
2020-03-29 15:45:02 +0300

    Wrote some more to front page info

--- src/components/FrontPageInfo.js
+++ src/components/FrontPageInfo.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const FrontPageInfo =()=>{
+const FrontPageInfo = () => {
 
   return (
     <div className="frontpage-info">
       <h2>Kisatehtäväpankki</h2>
       <p>Kisatehtäväpankki on arkisto,
-         johon voidaan tallettaa tehtäviä.
-         Sivustolle voit tallettaa tehtävän,
-         sekä siihen liittyvät liitetiedostot.
+      johon voidaan tallettaa tehtäviä.
+      Sivustolle voi tallettaa tehtävän,
+      sekä siihen liittyvät liitetiedostot.
       </p>
-      <br/>
       <p>
         Kirjautumattomana käyttäjänä voit
         koostaa tehtävistä kisaa varten
-        kokonaisuuksia, sekä ladata ne 
-        PDF-tiedostona itsellesi.
+        kokonaisuuksia, sekä ladata ne
+        PDF-tiedostona itsellesi. Tehtävän lisääminen "kisakoriin"
+        onnistuu painamalla tehtävässä esiintyvää koripainiketta,
+        ja jo mukaan otetut tehtävät löydät yläreunan koria painamalla.
       </p>
-      <br/>
       <p>
         Voit myöskin selailla tehtäviä, arvostella tehtäviä
         ja lisätä uusia tehtäviä. Uuden tehtävän lisääminen arkistoon
