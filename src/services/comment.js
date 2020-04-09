972b4b32edf77e298df920d45369debdb3369240
NooraVino <noora.virolainen@yahoo.com>
2020-04-09 14:43:40 +0300

    pending

--- src/services/comment.js
+++ src/services/comment.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import tokenService from './token'
 
 const baseUrl = `${process.env.REACT_APP_API_URL}/comment`
 
@@ -7,8 +8,16 @@ const getComments = async () => {
   return response.data
 }
 const addComment = async (comment) => {
+  let config = null
+  const token = tokenService.getToken()
+
+  if (token) {
+    config = {
+      headers: { Authorization: token }
+    }
+  }
  
-  const response = await axios.post(baseUrl, comment)
+  const response = await axios.post(baseUrl, comment, config)
   return response.data
 }
 
